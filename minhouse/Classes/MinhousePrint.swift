1899b8fd30ce62593f6cd24e5dc036909c3d8131
kimminjib <kimminjib@gmail.com>
2024-04-11 16:19:37 +0900

    zxzxzx

---
+++ minhouse/Classes/MinhousePrint.swift
@@ -0,0 +1,15 @@
+//
+//  MinhousePrint.swift
+//  minhouseutil
+//
+//  Created by Minhouse on 2024/04/11.
+//
+
+import UIKit
+
+public class MinhousePrint: NSObject {
+    public class func printDateTime() {
+        print(Date()) ;
+    }
+
+}
